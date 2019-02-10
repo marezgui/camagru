5bcf070b51cd38b0af7561a191244fb4f1916e19
Malek REZGUI <marezgui@e2r1p10.42.fr>
2019-02-10 03:30:34 +0100

    motage for mobile

--- views/montage.php
+++ views/montage.php
@@ -4,60 +4,61 @@
 ?>
 <?php ob_start(); ?>
 
-<section id="main_montage">
-	<section id="pannel">
-		<main id="camera">
-			<video id="video"></video>
-			<canvas id="canvas"></canvas>		
-			<button id="take">Prendre une photo</button>
-			<i>Ajouter Photo</i>
-			<input id="imageLoader" type="file"/>
-			<div id="prev"></div>
-			<i>Ajouter Filtre</i>
-			<form method="post" id="form" enctype="multipart/form-data">
-			    <input type="file" id="add_filtre" name="files[]" multiple>
-			    <input type="submit" value="Upload File" name="submit">
-			</form>
-		</main>
+<section id="montage-wrap">
+	<main id="camera" >
+		<video id="video"></video>
+		<canvas width="100%" height="100%" id="canvas"></canvas>		
+		<button id="take"><i class="fas fa-camera"></i> Prendre une photo</button>
 
-		<div >
-			<ul class="navigation_diapo" id="listfiltre">
-				<?php
+		<i>Ajouter Photo</i>
+		<input id="imageLoader" type="file"/>
+		<div id="prev"></div>
+
+		<i>Ajouter Filtre</i>
+		<form method="post" id="form" enctype="multipart/form-data">
+		    <input type="file" id="add_filtre" name="files[]" multiple>
+		    <input type="submit" value="Upload File" name="submit">
+		</form>
+	</main>
+
+	<div >
+		<ul class="navigation_diapo" id="listfiltre">
+			<?php
 				$path = $_SERVER['DOCUMENT_ROOT']."/camagru/public/images/filtre";
 				$dir = array_diff(scandir($path), array('..', '.'));
 				foreach ($dir as $key => $value) 
 				{
 					$filtre = explode('.', $value);
-				?>
+			?>
 					<li>
-					<a>
-					<img class='filtre' id='<?= $filtre[0] ?>' src='<?= "/camagru/public/images/filtre/". $value ?>'alt>
-					</a>
+						<a>
+							<img class='filtre' id='<?= $filtre[0] ?>' src='<?= "/camagru/public/images/filtre/". $value ?>'alt>
+						</a>
 					</li>
-				<?php				
-					}
-				?>
-			</ul>
-		</div>
-	</section>	
+			<?php				
+				}
+			?>
+		</ul>
+	</div>
 </section>
+
 <aside id="myImages">
-		<?php
-			$gallery = new Gallery();
-			$img = $gallery->getUserImage($_SESSION['login']);
-		?>
-		<?php 
-			for ($i= 0; $i < count($img); $i++)
-			{
-		?>
-				<div class="container">
+	<?php
+		$gallery = new Gallery();
+		$img = $gallery->getUserImage($_SESSION['login']);
+
+		for ($i= 0; $i < count($img); $i++)
+		{
+	?>
+			<div class="container">
 				<img src='<?= "/camagru/public/images/gallery/". $img[$i]['path'] ?>' />
-				<button class="btn" id='<?= $img[$i]['path'] ?>'>Delete</button>
-				</div>
-		<?php
-			}
-		?>
+				<button class="btn" id='<?= $img[$i]['path'] ?>'>Supprimer</button>
+			</div>
+	<?php
+		}
+	?>
 </aside>
+
 <script src="/camagru/public/js/montage.js"></script>
 
 <?php 
